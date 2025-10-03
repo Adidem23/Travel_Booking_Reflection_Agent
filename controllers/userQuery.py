cb8851b4779976b9a3d0a5b0db108248e3c73034
Adidem23 <adityavanshi5451@gmail.com>
2025-10-03 16:56:25 +0530

    fix:sse in mcp

--- controllers/userQuery.py
+++ controllers/userQuery.py
@@ -2,7 +2,7 @@ from dotenv import load_dotenv
 from fastapi import APIRouter
 from views.userQuery import requetsedQuery
 from AgenticPattern.reflection_agent import ReflectionAgent
-from mcp.client.sse import SSEClient
+
 
 load_dotenv()
 
@@ -14,30 +14,26 @@ def send_breating_msg():
 
 @router.post("/agentResponse")
 async def process_Agent_Response(userQuery:requetsedQuery):
-        async with SSEClient("http://localhost:1820/mcp") as client:
-                result = await client.call_tool("get_weather", {"destination": "Pune"})
-                return result
-
-        # reflection_Agent=ReflectionAgent()
+        reflection_Agent=ReflectionAgent()
         
-        # generation_system_prompt = "You are a travel booking agent which creates a best plan according to the user requirements"
+        generation_system_prompt = "You are a travel booking agent which creates a best plan according to the user requirements"
 
-        # reflection_system_prompt = """You are a Reflection Agent.
-        # Review the Trip Agent’s itinerary and provide structured feedback.
-        # Check for completeness (transport, lodging, activities, weather).
-        # Check for accuracy (dates, times, feasibility).
-        # Check for user experience (local attractions, budget, upgrades).
-        # Point out issues clearly and suggest actionable improvements.
-        # Do not regenerate the plan—only critique and refine.
-        # """
+        reflection_system_prompt = """You are a Reflection Agent.
+        Review the Trip Agent’s itinerary and provide structured feedback.
+        Check for completeness (transport, lodging, activities, weather).
+        Check for accuracy (dates, times, feasibility).
+        Check for user experience (local attractions, budget, upgrades).
+        Point out issues clearly and suggest actionable improvements.
+        Do not regenerate the plan—only critique and refine.
+        """
 
-        # user_msg =userQuery.actualQueryString
+        user_msg =userQuery.actualQueryString
 
-        # final_response = await reflection_Agent.run(
-        #     user_msg=user_msg,
-        #     generation_system_prompt=generation_system_prompt,
-        #     reflection_system_prompt=reflection_system_prompt,
-        #     n_steps=10
-        # )
+        final_response = await reflection_Agent.run(
+            user_msg=user_msg,
+            generation_system_prompt=generation_system_prompt,
+            reflection_system_prompt=reflection_system_prompt,
+            n_steps=10
+        )
 
-        # return final_response
+        return final_response
